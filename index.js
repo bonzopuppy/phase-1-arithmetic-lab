b1ea9adae2fb77e6657b93c1254a8580ed5fc1e5
David Baden <davebaden@gmail.com>
2023-10-31 09:09:56 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 44;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5,10,15,20);
